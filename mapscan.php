8539d7082339352bd229d2acc3e1b3cb23e3da77
Andarch <162850255+Andarch@users.noreply.github.com>
2024-07-07 13:02:38 -0500

    Move datatables function to .js file

--- mapscan.php
+++ mapscan.php
@@ -324,34 +324,6 @@ if($mapok) {
 }
 
 ?>
-<script type="text/javascript">
-$(document).ready(function() {
-    var objectsTable = $('#objectsTable').DataTable({
-        "paging": false,
-        "info": false,
-        "ordering": true,
-        "searching": true,
-        "columnDefs": [
-            { "orderable": false, "targets": 0 }
-        ],
-        "initComplete": function(settings, json) {
-			new $.fn.dataTable.FixedHeader(this.api());
-		}
-    });
-
-    var townDetailsTable = $('#townDetailsTable').DataTable({
-        "paging": false,
-        "info": false,
-        "ordering": true,
-        "searching": true,
-        "columnDefs": [
-            { "orderable": false, "targets": 0 }
-        ],
-        "initComplete": function(settings, json) {
-			new $.fn.dataTable.FixedHeader(this.api());
-		}
-    });
-});
-</script>
+<script type="application/javascript" src="js/datatables.js"></script>
 </body>
 </html>
