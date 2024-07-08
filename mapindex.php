58a7589c82a1c5df92c8c466b72ad74f8bc34fc2
Andarch <162850255+Andarch@users.noreply.github.com>
2024-07-07 19:45:15 -0500

    Prevent browser caching

--- mapindex.php
+++ mapindex.php
@@ -1,4 +1,9 @@
 <?php
+// Prevent caching
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
 
 require_once 'src/mi.php';
 require_once 'src/config.php';
--- mapscan.php
+++ mapscan.php
@@ -1,4 +1,10 @@
 <?php
+// Prevent caching
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+
 header('Content-Type: text/html; charset=utf-8');
 
 require_once 'src/mi.php';
