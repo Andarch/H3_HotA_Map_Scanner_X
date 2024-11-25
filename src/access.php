ed2f9a947f0c2edbefa3b44a764ede2aaf16444e
Andarch <andarchitect1@gmail.com>
2024-11-25 15:34:00 -0600

    Updated access

--- src/access.php
+++ src/access.php
@@ -2,9 +2,9 @@
 	//mysql access
 	global $_cfg;
 		$_cfg = array(
-		'mq.host' => 'localhost',
-		'mq.user' => 'root',
-		'mq.pass' => '',
-		'mq.db'   => 'heroesdb',
+		'mq.host' => 'sql311.infinityfree.com',
+		'mq.user' => 'if0_37786384',
+		'mq.pass' => 'Alamar85',
+		'mq.db'   => 'if0_37786384_h3db',
 		'mq.port' => '3306',
 	);
