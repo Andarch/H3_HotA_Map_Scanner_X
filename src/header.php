be6d994fe379d4a884afe4446c98f1b43dd25970
Andarch <andarchitect1@gmail.com>
2024-12-07 20:04:50 -0600

    More header

--- src/header.php
+++ src/header.php
@@ -4,6 +4,27 @@ function generateHeader() {
     ?>
 
     <div class="site-header">
+        <table>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+
+    <?php
+
+    ?>
+
     </div>
 
     <?php
