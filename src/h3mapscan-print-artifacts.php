f02f1d16883b9fe248463ec3d2bcc8839194b7e4
Andarch <162850255+Andarch@users.noreply.github.com>
2025-06-23 17:59:56 -0500

    Updates

--- src/h3mapscan-print-artifacts.php
+++ src/h3mapscan-print-artifacts.php
@@ -1,57 +1,216 @@
 <?php
 /** @var H3MAPSCAN_PRINT $this */
 
-$artifactGroups = [];
+$artifacts_all = [
+	7 => 'Centaur Axe',
+	8 => 'Blackshard of the Dead Knight',
+	9 => 'Greater Gnoll\'s Flail',
+	10 => 'Ogre\'s Club of Havoc',
+	11 => 'Sword of Hellfire',
+	12 => 'Titan\'s Gladius',
+	13 => 'Shield of the Dwarven Lords',
+	14 => 'Shield of the Yawning Dead',
+	15 => 'Buckler of the Gnoll King',
+	16 => 'Targ of the Rampaging Ogre',
+	17 => 'Shield of the Damned',
+	18 => 'Sentinel\'s Shield',
+	19 => 'Helm of the Alabaster Unicorn',
+	20 => 'Skull Helmet',
+	21 => 'Helm of Chaos',
+	22 => 'Crown of the Supreme Magi',
+	23 => 'Hellstorm Helmet',
+	24 => 'Thunder Helmet',
+	25 => 'Breastplate of Petrified Wood',
+	26 => 'Rib Cage',
+	27 => 'Scales of the Greater Basilisk',
+	28 => 'Tunic of the Cyclops King',
+	29 => 'Breastplate of Brimstone',
+	30 => 'Titan\'s Cuirass',
+	31 => 'Armor of Wonder',
+	32 => 'Sandals of the Saint',
+	33 => 'Celestial Necklace of Bliss',
+	34 => 'Lion\'s Shield of Courage',
+	35 => 'Sword of Judgement',
+	36 => 'Helm of Heavenly Enlightenment',
+	37 => 'Quiet Eye of the Dragon',
+	38 => 'Red Dragon Flame Tongue',
+	39 => 'Dragon Scale Shield',
+	40 => 'Dragon Scale Armor',
+	41 => 'Dragonbone Greaves',
+	42 => 'Dragon Wing Tabard',
+	43 => 'Necklace of Dragonteeth',
+	44 => 'Crown of Dragontooth',
+	45 => 'Still Eye of the Dragon',
+	46 => 'Clover of Fortune',
+	47 => 'Cards of Prophecy',
+	48 => 'Ladybird of Luck',
+	49 => 'Badge of Courage',
+	50 => 'Crest of Valor',
+	51 => 'Glyph of Gallantry',
+	52 => 'Speculum',
+	53 => 'Spyglass',
+	54 => 'Amulet of the Undertaker',
+	55 => 'Vampire\'s Cowl',
+	56 => 'Dead Man\'s Boots',
+	57 => 'Garniture of Interference',
+	58 => 'Surcoat of Counterpoise',
+	59 => 'Boots of Polarity',
+	60 => 'Bow of Elven Cherrywood',
+	61 => 'Bowstring of the Unicorn\'s Mane',
+	62 => 'Angel Feather Arrows',
+	63 => 'Bird of Perception',
+	64 => 'Stoic Watchman',
+	65 => 'Emblem of Cognizance',
+	66 => 'Statesman\'s Medal',
+	67 => 'Diplomat\'s Ring',
+	68 => 'Ambassador\'s Sash',
+	69 => 'Ring of the Wayfarer',
+	70 => 'Equestrian\'s Gloves',
+	71 => 'Necklace of Ocean Guidance',
+	72 => 'Angel Wings',
+	73 => 'Charm of Mana',
+	74 => 'Talisman of Mana',
+	75 => 'Mystic Orb of Mana',
+	76 => 'Collar of Conjuring',
+	77 => 'Ring of Conjuring',
+	78 => 'Cape of Conjuring',
+	79 => 'Orb of the Firmament',
+	80 => 'Orb of Silt',
+	81 => 'Orb of Tempestuous Fire',
+	82 => 'Orb of Driving Rain',
+	83 => 'Recanter\'s Cloak',
+	84 => 'Spirit of Oppression',
+	85 => 'Hourglass of the Evil Hour',
+	86 => 'Tome of Fire Magic',
+	87 => 'Tome of Air Magic',
+	88 => 'Tome of Water Magic',
+	89 => 'Tome of Earth Magic',
+	90 => 'Boots of Levitation',
+	91 => 'Golden Bow',
+	92 => 'Sphere of Permanence',
+	93 => 'Orb of Vulnerability',
+	94 => 'Ring of Vitality',
+	95 => 'Ring of Life',
+	96 => 'Vial of Lifeblood',
+	97 => 'Necklace of Swiftness',
+	98 => 'Boots of Speed',
+	99 => 'Cape of Velocity',
+	100 => 'Pendant of Dispassion',
+	101 => 'Pendant of Second Sight',
+	102 => 'Pendant of Holiness',
+	103 => 'Pendant of Life',
+	104 => 'Pendant of Death',
+	105 => 'Pendant of Free Will',
+	106 => 'Pendant of Negativity',
+	107 => 'Pendant of Total Recall',
+	108 => 'Pendant of Courage',
+	109 => 'Everflowing Crystal Cloak',
+	110 => 'Ring of Infinite Gems',
+	111 => 'Everpouring Vial of Mercury',
+	112 => 'Inexhaustible Cart of Ore',
+	113 => 'Eversmoking Ring of Sulfur',
+	114 => 'Inexhaustible Cart of Lumber',
+	115 => 'Endless Sack of Gold',
+	116 => 'Endless Bag of Gold',
+	117 => 'Endless Purse of Gold',
+	118 => 'Legs of Legion',
+	119 => 'Loins of Legion',
+	120 => 'Torso of Legion',
+	121 => 'Arms of Legion',
+	122 => 'Head of Legion',
+	123 => 'Sea Captain\'s Hat',
+	124 => 'Spellbinder\'s Hat',
+	125 => 'Shackles of War',
+	126 => 'Orb of Inhibition',
+	127 => 'Vial of Dragon Blood',
+	128 => 'Armageddon\'s Blade',
+	129 => 'Angelic Alliance',
+	130 => 'Cloak of the Undead King',
+	131 => 'Elixir of Life',
+	132 => 'Armor of the Damned',
+	133 => 'Statue of Legion',
+	134 => 'Power of the Dragon Father',
+	135 => 'Titan\'s Thunder',
+	136 => 'Admiral\'s Hat',
+	137 => 'Bow of the Sharpshooter',
+	138 => 'Wizard\'s Well',
+	139 => 'Ring of the Magi',
+	140 => 'Cornucopia',
+	141 => 'Diplomat\'s Cloak',
+	142 => 'Pendant of Reflection',
+	143 => 'Ironfist of the Ogre',
+	146 => 'Cannon',
+	147 => 'Trident of Dominion',
+	148 => 'Shield of Naval Glory',
+	149 => 'Royal Armor of Nix',
+	150 => 'Crown of the Five Seas',
+	151 => 'Wayfarer\'s Boots',
+	152 => 'Runes of Imminency',
+	153 => 'Demon\'s Horseshoe',
+	154 => 'Shaman\'s Puppet',
+	155 => 'Hideous Mask',
+	156 => 'Ring of Suppression',
+	157 => 'Pendant of Downfall',
+	158 => 'Ring of Oblivion',
+	159 => 'Cape of Silence',
+	160 => 'Golden Goose',
+	161 => 'Horn of the Abyss',
+	162 => 'Charm of Eclipse',
+	163 => 'Seal of Sunset',
+	164 => 'Plate of Dying Light',
+	165 => 'Sleepkeeper',
+];
 
-foreach ($this->h3mapscan->artifacts_list as $art) {
-	if(
-		substr($art->name, 0, 6) != 'Random'
-		&& $art->name != 'Spell Book'
-		&& $art->name != 'Ammo Cart'
-		&& $art->name != 'Ballista'
-		&& $art->name != 'Cannon'
-		&& $art->name != 'First Aid Tent') {
-		if (!isset($artifactGroups[$art->name])) {
-			$artifactGroups[$art->name] = [
-				'artifactname' => $art->name,
-				'mapobjs' => [],
-				'miscmapobjs' => [],
-				'heroes' => []
-			];
-		}
-		
-		if ($art->parent === 'Map') {
-			$location = 'Artifact ' . $art->mapcoor->GetCoords();
-			$artifactGroups[$art->name]['mapobjs'][] = $location;
-		} else if ($art->parent != 'Hero') {
-			if($art->parent == 'Monster') {
-				$name = $art->add1;
-			} else {
-				$name = $art->parent;
+$disabledArtifacts = $this->h3mapscan->disabledArtifacts;
+
+// Group by location
+$artifactLocations = [];
+foreach ($artifacts_all as $artid => $artname) {
+	if (!isset($artifactLocations[$artname])) {
+		$artifactLocations[$artname] = [
+			'artifactname' => $artname,
+			'artifactid' => $artid,
+			'mapobjs' => [],
+			'miscmapobjs' => [],
+			'heroes' => []
+		];
+	}
+	foreach ($this->h3mapscan->artifacts_list as $mapartifact) {
+		if($mapartifact->name == $artname) {
+			if ($mapartifact->parent === 'Map') {
+				$artifactLocations[$mapartifact->name]['mapobjs'][] = 'Artifact ' . $mapartifact->mapcoor->GetCoords();
+			} else if ($mapartifact->parent != 'Hero') {
+				if($mapartifact->parent == 'Monster') {
+					$name = $mapartifact->add1;
+				} else {
+					$name = $mapartifact->parent;
+				}
+				$location = $name . ' ' . $mapartifact->mapcoor->GetCoords();
+				$artifactLocations[$mapartifact->name]['miscmapobjs'][] = $location;
+			} else if ($mapartifact->parent === 'Hero') {
+				$location = $mapartifact->add1 . ' ' . $mapartifact->mapcoor->GetCoords();
+				$artifactLocations[$mapartifact->name]['heroes'][] = $location;
 			}
-			$location = $name . ' ' . $art->mapcoor->GetCoords();
-			$artifactGroups[$art->name]['miscmapobjs'][] = $location;
-		} else if ($art->parent === 'Hero') {
-			$location = $art->add1 . ' ' . $art->mapcoor->GetCoords();
-			$artifactGroups[$art->name]['heroes'][] = $location;
 		}
 	}
 }
 
 // Create consolidated array
 $consolidatedData = [];
+const NONE = '<span class="obj-count-inactive">None</span>';
 
-foreach ($artifactGroups as $group) {
+foreach ($artifactLocations as $group) {
     sort($group['mapobjs']);
     sort($group['miscmapobjs']);
     sort($group['heroes']);
     
-    $mapobjsText = count($group['mapobjs']) > 0 ? implode('</br>', $group['mapobjs']) : '<span class="tiny-grey-italics">None</span>';
-    $miscmapobjsText = count($group['miscmapobjs']) > 0 ? implode('</br>', $group['miscmapobjs']) : '<span class="tiny-grey-italics">None</span>';
-    $heroesText = count($group['heroes']) > 0 ? implode('</br>', $group['heroes']) : '<span class="tiny-grey-italics">None</span>';
+    $mapobjsText = count($group['mapobjs']) > 0 ? implode('</br>', $group['mapobjs']) : NONE;
+    $miscmapobjsText = count($group['miscmapobjs']) > 0 ? implode('</br>', $group['miscmapobjs']) : NONE;
+    $heroesText = count($group['heroes']) > 0 ? implode('</br>', $group['heroes']) : NONE;
     
     $consolidatedData[] = [
         'name' => $group['artifactname'],
+		'id' => '5-'.$group['artifactid'],
         'mapobjs' => $mapobjsText,
         'miscmapobjs' => $miscmapobjsText,
         'heroes' => $heroesText
@@ -63,32 +222,58 @@ usort($consolidatedData, function($a, $b) {
     return strcmp($a['name'], $b['name']);
 });
 
-$totalItems = count($consolidatedData);
+// Split by group
+$artifactGroups = [];
+$artifactGroups['Disabled Artifacts'] = [];
+$artifactGroups['Enabled Artifacts'] = [];
+foreach ($consolidatedData as $art) {
+	if (in_array($art['name'], $disabledArtifacts)) {
+		$artifactGroups['Disabled Artifacts'][] = $art;
+	} else {
+		$artifactGroups['Enabled Artifacts'][] = $art;
+	}
+}
 
+// Print
 echo '<div class="flex-container">';
 
-echo '<table class="table-large artifacts-table">
-			<thead>
-				<tr>
-					<th>#</th>
-					<th>Artifact</th>
-					<th>Map Objects</th>
-					<th>Misc. Map Objects</th>
-					<th>Heroes</th>
-				</tr>
-			</thead>
-			<tbody>';
-for ($n = 0; $n < $totalItems; $n++) {
-	$art = $consolidatedData[$n];
-	echo '<tr>
-			<td class="table__row-header--default">'.($n + 1).'</td>
-			<td class="nowrap" nowrap="nowrap">'.$art['name'].'</td>
-			<td class="nowrap" nowrap="nowrap"><span class="small-text">'.$art['mapobjs'].'</span></td>
-			<td class="nowrap" nowrap="nowrap"><span class="small-text">'.$art['miscmapobjs'].'</span></td>
-			<td class="nowrap" nowrap="nowrap"><span class="small-text">'.$art['heroes'].'</span></td>
+foreach($artifactGroups as $groupName => $artifacts) {
+
+	echo '<table class="table-small artifacts-table">
+				<thead>
+					<tr>
+						<th class="table__title-bar--small" colspan="6">'.$groupName.'</td>
+					</tr>
+					<tr>
+						<th>#</th>
+						<th style="width: 1px !important;">ID</th>
+						<th>Artifact</th>
+						<th>Map Objects</th>
+						<th>Misc. Map Objects</th>
+						<th>Heroes</th>
+					</tr>
+				</thead>
+				<tbody>';
+	for ($n = 0; $n < count($artifacts); $n++) {
+		$art = $artifacts[$n];
+
+		$mapobjsClass = $art['mapobjs'] == NONE ? ' obj-count-inactive' : ' obj-count-active';	
+		$miscmapobjsClass = $art['miscmapobjs'] == NONE ? ' obj-count-inactive' : ' obj-count-active';
+		$heroesClass = $art['heroes'] == NONE ? ' obj-count-inactive' : ' obj-count-active';
+		$artClass = $art['mapobjs'] == NONE && $art['miscmapobjs'] == NONE && $art['heroes'] == NONE ? ' obj-count-inactive' : ' obj-count-active';
+
+		echo '<tr>
+				<td class="table__row-header--default">'.($n + 1).'</td>
+				<td class="ac nowrap'.$artClass.'" nowrap="nowrap" style="font-size: 10px; width: 1px !important;">'.$art['id'].'</td>
+				<td class="nowrap'.$artClass.'" nowrap="nowrap">'.$art['name'].'</td>
+				<td class="nowrap'.$mapobjsClass.'" nowrap="nowrap"><span class="small-text">'.$art['mapobjs'].'</span></td>
+				<td class="nowrap'.$miscmapobjsClass.'" nowrap="nowrap"><span class="small-text">'.$art['miscmapobjs'].'</span></td>
+				<td class="nowrap'.$heroesClass.'" nowrap="nowrap"><span class="small-text">'.$art['heroes'].'</span></td>
 			</tr>';
+	}
+	echo '</tbody>';
+	echo '</table>';
+	
 }
-echo '</tbody>';
-echo '</table>';
 
 echo '</div>';
