ff95c0a8ac9001254df73d7258ce315c62f41915
Andarch <162850255+Andarch@users.noreply.github.com>
2025-10-31 21:36:03 -0500

    More Garrisons

--- src/h3mapscan-print-garrisons.php
+++ src/h3mapscan-print-garrisons.php
@@ -12,7 +12,7 @@
                 <th>Position</th>
                 <th>Owner</th>
                 <th class="nowrap" nowrap="nowrap">Troops are<br />Removable</th>
-                <th>Guards</th>
+                <th colspan="7">Guards</th>
             </tr>
         </thead>
     </table>
@@ -39,7 +39,15 @@
                     <td class="ac nowrap" nowrap="nowrap"><?= $garrison->mapcoor->GetCoords() ?></td>
                     <td class="ac nowrap" nowrap="nowrap"><?= $owner ?></td>
                     <td class="ac nowrap" nowrap="nowrap"><?= $garrison->add1 ?></td>
-                    <td class="nowrap" nowrap="nowrap"><?= $this->h3mapscan->PrintStack($garrison->add2) ?></td>
+                    <?php
+                    for ($i = 0; $i < 7; $i++) {
+                        ?>
+                        <td class="nowrap" nowrap="nowrap">
+                            <?= isset($garrison->add2[$i]) ? $this->h3mapscan->GetCreatureById($garrison->add2[$i]["id"]) . ": " . comma($garrison->add2[$i]["count"]) : "" ?>
+                        </td>
+                        <?php
+                    }
+                    ?>
                 </tr>
                 <?php
             }
