891b88201fc76d27f517769c37744a456cf9c823
Andarch <162850255+Andarch@users.noreply.github.com>
2026-02-10 09:04:57 -0600

    Again

--- src/h3mapscan-print-pandoras.php
+++ src/h3mapscan-print-pandoras.php
@@ -19,12 +19,12 @@ foreach ($this->h3mapscan->events_list as $evento) {
 	if ($evento['objname'] == 'Pandora\'s Box') {
 		$event = $evento['data'];
 
-		$stack = '';
+		$guards = '';
 		$msg = '';
 		if (!empty($event['MessageStack'])) {
 			$msg = nl2br($event['MessageStack']['message']);
-			if (array_key_exists('stack', $event['MessageStack'])) {
-				$stack = $this->h3mapscan->PrintStack($event['MessageStack']['stack']);
+			if ($event['MessageStack']['guards'] != EMPTY_DATA) {
+				$guards = $this->h3mapscan->PrintStack($event['MessageStack']['guards']);
 			}
 		}
 
@@ -41,7 +41,7 @@ foreach ($this->h3mapscan->events_list as $evento) {
 			echo '<td class="ac nowrap zone-type" nowrap="nowrap" data-zone="' . htmlspecialchars($evento['zone_type'], ENT_QUOTES, 'UTF-8') . '">' . htmlspecialchars($evento['zone_type'], ENT_QUOTES, 'UTF-8') . '</td>';
 		}
 		echo '<td class="ac" style="width:120px;">' . implode(', ', $event['difficulty']) . '</td>
-			<td class="small-text nowrap" nowrap="nowrap">' . $stack . '</td>
+			<td class="small-text nowrap" nowrap="nowrap">' . $guards . '</td>
 			<td class="small-text thin-vertical-border nowrap" nowrap="nowrap">' . implode('<br />', $content[1]) . '</td>
 			<td class="small-text thin-vertical-border nowrap" nowrap="nowrap">' . implode('<br />', $content[2]) . '</td>
 			<td class="small-text thin-vertical-border nowrap" nowrap="nowrap">' . implode('<br />', $content[3]) . '</td>
